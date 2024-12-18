455f53c5030c578693e74215e8b4b1fb991f8953
DaurenMash <doxa@inbox.ru>
2024-12-18 14:22:35 +0300

    UserServiceImpl.class

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,33 +8,33 @@ import java.util.List;
 
 
 public class UserServiceImpl extends UserDaoHibernateImpl implements UserService {
-    UserDao userDaoHibernate = new UserDaoHibernateImpl();
+    UserDao userDao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        userDaoHibernate.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDaoHibernate.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        userDaoHibernate.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) {
-        userDaoHibernate.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDaoHibernate.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDaoHibernate.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 
 
