4a5a9b6b7e592112c45bbd85ec1bcb7b52174456
DaurenMash <doxa@inbox.ru>
2024-12-19 19:05:06 +0300

    UserServiceImpl.class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,5 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
@@ -18,27 +15,16 @@ public class Util {
     private static final String DB_PASSWORD = "root";
     private static SessionFactory sessionFactory;
 
-//    public static Connection getConnection() {
-//        Connection connection = null;
-//        try {
-//            Class.forName(DB_DRIVER);
-//            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//            System.out.println("Failed to connect to database");
-//        }
-//        return connection;
-//    }
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
                 Configuration configuration = new Configuration();
                 Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/myusers");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DRIVER, DB_DRIVER);
+                settings.put(Environment.URL, DB_URL);
+                settings.put(Environment.USER, DB_USER);
+                settings.put(Environment.PASS, DB_PASSWORD);
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
@@ -57,4 +43,8 @@ public class Util {
         }
         return sessionFactory;
     }
+
+
+
+
 }
